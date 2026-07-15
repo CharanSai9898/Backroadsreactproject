77b47663ddd74f532447588f7f7670385b98f26f
Koppisetti Charan Sai <koppisetticharansai@gmail.com>
2026-07-15 14:35:42 +0530

    changes made in data

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ export const pageLinks = [
 export const socialLinks = [
   { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
-  { id: 3, href: "https://www.instagram.com", icon: "fa-brands fa-instagram" },
+  { id: 3, href: "https://www.instagram.com", icon: "fab fa-instagram" },
 ];
 
 export const services = [
